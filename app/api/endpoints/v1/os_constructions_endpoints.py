6f11f8126ffcfa632eedd303192eee2613d20401
ivan <ivancampelo1973@gmail.com>
2024-01-16 07:35:19 -0300

    feat: endpoint para atualizar os de obra

--- app/api/endpoints/v1/os_constructions_endpoints.py
+++ app/api/endpoints/v1/os_constructions_endpoints.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
 
 
-from app.schemas.os_constructions_schemas import OsConstructionsCreate
+from app.schemas.os_constructions_schemas import OsConstructionsCreate, OsConstructionsUpdate
 from app.models.os_constructions_models import OsConstructions
 from app.auth.auth_bearer_employee import JWTBearerEmployee
 from app.auth.auth_handle import token_employee_required
@@ -93,6 +93,84 @@ async def get_one_os_constructions(dependencies=Depends(JWTBearerEmployee()), os
             obj_os_construction = os_constructions_id.scalar_one()
             return obj_os_construction
     except Exception as e:
+        raise HTTPException(status_code=500, detail=f"{e}")
+    
+    
+@token_employee_required
+@async_session
+@router.put('/update-os-constructions/{os_constructions_id}', responses={
+    200: {
+        "description": "Ordem de Serviço para obra atualizada com sucesso",
+        "content": {
+            "application/json": {
+                "example": [
+                    {   
+                        "employee_id": 1,
+                        "client_id": 1,
+                        "client_adress_id": 1, 
+                        "checklist_cam_id": 1, 
+                        "checklist_auto_id": 1, 
+                        "checklist_sound_id": 1, 
+                        "other_checklist_id": 1, 
+                        "os_type": "Instalação",
+                        "scheduling": "2024-01-01",
+                        "end_date": "2024-01-12",
+                        "info": "instalar duas câmeras",
+                        "solution": "As duas câmeras foram instaladas",
+                        "sale": "O cliente precisa de um hd maior",
+                        "signature_emplooye": "Maria",
+                        "signature_client": "João",
+                        "update_at": "2024-01-03 10:12:49.17512",
+                        "created_at": "2024-01-03 10:12:49.17512",
+                        "is_active": True
+                    }
+
+                ]
+            }
+        },
+        404: {"description": "Insira dados válidos"}
+}}, status_code=status.HTTP_202_ACCEPTED)
+async def update_os_construction(os_construction_id: int, os_construction_update: OsConstructionsUpdate, session: AsyncSession = Depends(conn.get_async_session)):
+    try:
+        async with session.begin():
+            os = await session.execute(select(OsConstructions).where(OsConstructions.id == os_construction_id))
+            existing_os = os.scalars().first()
+
+            if existing_os:
+                if os_construction_update.employee_id is not None:
+                    existing_os.employee_id = os_construction_update.employee_id
+                else: 
+                    existing_os.employee_id = existing_os.employee_id
+            
+                if os_construction_update.client_id is not None: 
+                    existing_os.client_id = os_construction_update.client_id
+                else: 
+                    existing_os.client_id = existing_os.client_id
+
+                if os_construction_update.construction_id is not None:
+                    existing_os.construction_id = os_construction_update.construction_id
+                else: 
+                    existing_os.construction_id = existing_os.construction_id
+
+
+                existing_os.checklist_cam_id = os_construction_update.checklist_cam_id
+                existing_os.checklist_auto_id = os_construction_update.checklist_auto_id
+                existing_os.checklist_sound_id = os_construction_update.checklist_sound_id
+                existing_os.other_checklist_id = os_construction_update.other_checklist_id
+                existing_os.scheduling = os_construction_update.scheduling 
+                existing_os.end_date = os_construction_update.end_date
+                existing_os.info = os_construction_update.info
+                existing_os.sale = os_construction_update.sale
+                existing_os.signature_emplooye = os_construction_update.signature_emplooye
+                existing_os.signature_client = os_construction_update.signature_client
+        
+                await session.commit()
+                return existing_os
+            else:
+                return {"message": "Ordem de Serviço para obra não encontrada"}
+            
+    except Exception as e:
+        await session.rollback()
         raise HTTPException(status_code=500, detail=f"{e}")
     
 
--- app/schemas/os_constructions_schemas.py
+++ app/schemas/os_constructions_schemas.py
@@ -7,6 +7,7 @@ class OsConstructionsBase(BaseModel):
     employee_id: int 
     client_id: int 
     construction_id: int
+    
 
 class OsConstructionsCreate(OsConstructionsBase):
     checklist_cam_id: int
@@ -21,5 +22,23 @@ class OsConstructionsCreate(OsConstructionsBase):
     signature_emplooye: str
     signature_client: str
 
+
 class OsConstructions:
     id: int
+
+
+class OsConstructionsUpdate(BaseModel): 
+    employee_id: Optional[int] = None
+    client_id: Optional[int] = None  
+    construction_id: Optional[int] = None
+    checklist_cam_id: Optional[int] = None 
+    checklist_auto_id: Optional[int] = None 
+    checklist_sound_id: Optional[int] = None 
+    other_checklist_id: Optional[int] = None 
+    scheduling: Optional[date] = None 
+    end_date: Optional[date] = None
+    info: Optional[str] = None
+    solution: Optional[str] = None 
+    sale: Optional[str] = None
+    signature_emplooye: Optional[str] = None 
+    signature_client: Optional[str] = None
